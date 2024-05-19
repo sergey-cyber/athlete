b6d8cb0ed0196dc5bce5c6e2a260a7a9ddef0f57
cyber-sergey <zenkovich_so@mail.ru>
2024-05-20 02:44:18 +0800

    merchandise list item

--- src/app/(shop)/merchandise/page.tsx
+++ src/app/(shop)/merchandise/page.tsx
@@ -1,3 +1,4 @@
+import { MerchendiseListItem } from "@/components/merchandises/merchandise-item";
 import { Button } from "@/components/ui/button";
 import { toCreateMerchandise } from "@/lib/routes";
 import { merchandiseService } from "@/service/merchandiseService";
@@ -10,7 +11,7 @@ export default async function MerchandisesPage() {
   return (
     <section className="space-y-6">
       <div className="py-6 flex justify-between">
-        <h1 className="text-xl font-bold">Каталог товаров</h1>
+        <h1 className="text-2xl font-bold">Каталог товаров</h1>
         <Button asChild>
           <Link href={toCreateMerchandise()}>
             <Plus className="mr-2" />
@@ -19,7 +20,11 @@ export default async function MerchandisesPage() {
         </Button>
       </div>
       {merchandises.length ? (
-        <div></div>
+        <div className="grid grid-cols-4 gap-4 lg:grid-cols-5 xl:grid-cols-6">
+          {merchandises.map((item) => (
+            <MerchendiseListItem key={item.id} item={item} />
+          ))}
+        </div>
       ) : (
         <div className="w-full flex flex-col justify-center items-center text-gray-500">
           <Inbox size={36} /> Пока ничего нет
--- src/components/merchandises/merchandise-item.tsx
+++ src/components/merchandises/merchandise-item.tsx
@@ -1,5 +1,31 @@
-import { Card } from "../ui/card";
+import { MerchandiseType } from "@/service/merchandiseService/types";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "../ui/card";
+import { Button } from "../ui/button";
 
-export function MerchendiseItem() {
-  return <Card></Card>;
+interface Props {
+  item: MerchandiseType;
+}
+
+export function MerchendiseListItem({ item }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-center">{item.title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{item.description}</CardDescription>
+        <p>{item.price}</p>
+      </CardContent>
+      <CardFooter className="justify-center">
+        <Button variant={"outline"}>В корзину</Button>
+      </CardFooter>
+    </Card>
+  );
 }
