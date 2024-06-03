6dca57d0fdfa1dd7b8c6b7fa000d0d314aaaf4a4
zenkovichso <zenkovich_so@mail.ru>
2024-06-03 19:07:09 +0300

    AmenitiesForm has been unified

--- src/components/amenities/amenities-form.tsx
+++ src/components/amenities/amenities-form.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren } from "react";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { AmenitiesType } from "@/service/amenities/types";
+import { Label } from "../ui/label";
 
 interface Props extends PropsWithChildren {
   values: Partial<AmenitiesType>;
@@ -13,23 +14,29 @@ interface Props extends PropsWithChildren {
 export function AmenitiesForm({ values, onChange, children }: Props) {
   return (
     <div className="flex flex-col w-1/3 gap-y-4 pt-4">
-      <Input
-        type="string"
-        placeholder="Название"
-        value={values.title}
-        onChange={(e) => onChange("title", e.currentTarget.value)}
-      />
-      <Textarea
-        placeholder="Описание"
-        value={values.description}
-        onChange={(e) => onChange("description", e.currentTarget.value)}
-      />
-      <Input
-        type="number"
-        placeholder="Стоимость"
-        value={values.price || ""}
-        onChange={(e) => onChange("price", e.currentTarget.value)}
-      />
+      <div>
+        <Label>Название</Label>
+        <Input
+          type="string"
+          value={values.title}
+          onChange={(e) => onChange("title", e.currentTarget.value)}
+        />
+      </div>
+      <div>
+        <Label>Описание</Label>
+        <Textarea
+          value={values.description}
+          onChange={(e) => onChange("description", e.currentTarget.value)}
+        />
+      </div>
+      <div>
+        <Label>Стоимость</Label>
+        <Input
+          type="number"
+          value={values.price || ""}
+          onChange={(e) => onChange("price", e.currentTarget.value)}
+        />
+      </div>
       {/*Submit button placement*/}
       {children}
     </div>
