04844ddd76e8238c2ff62ce9d9570327ecca3316
zenkovichso <zenkovich_so@mail.ru>
2024-06-04 16:00:56 +0300

    global error screen fixed

--- src/app/(shop)/error.tsx
+++ src/app/(shop)/error.tsx
@@ -1,19 +1,14 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { toHome } from "@/lib/routes";
-import { Home } from "lucide-react";
-import Link from "next/link";
-
 export default function RootError() {
   return (
     <section className="w-full h-full flex flex-col items-center justify-center py-6 gap-y-6">
-      <h2 className="text-xl font-bold">Произошла ошибка!</h2>
-      <Button asChild>
+      <h2 className="text-xl font-bold">Что-то пошло не так!</h2>
+      {/* <Button asChild>
         <Link href={toHome()}>
           <Home className="mr-2" /> На главную
         </Link>
-      </Button>
+      </Button> */}
     </section>
   );
 }
