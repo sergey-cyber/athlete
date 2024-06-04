b2b097b98b6bf9ec02c6df6042cbc857aba855d2
zenkovichso <zenkovich_so@mail.ru>
2024-06-04 18:52:55 +0300

    numberInWarehouse in merchandiser card

--- src/components/product/product-list-item.tsx
+++ src/components/product/product-list-item.tsx
@@ -4,7 +4,7 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle
+  CardTitle,
 } from "../ui/card";
 import { RussianRuble } from "lucide-react";
 import { PropsWithChildren } from "react";
@@ -13,13 +13,15 @@ interface Props extends PropsWithChildren {
   title: string;
   description: string;
   price: number;
+  numberInWarehouse?: number;
 }
 
 export function ProductListItem({
   title,
   description,
   price,
-  children
+  children,
+  numberInWarehouse,
 }: Props) {
   return (
     <Card className="relative">
@@ -32,9 +34,15 @@ export function ProductListItem({
         <CardDescription className="line-clamp-3 min-h-16">
           {description}
         </CardDescription>
-        <div className="pt-2 flex items-center text-2xl font-semibold">
-          {price}
-          <RussianRuble className="pt-1" />
+        {numberInWarehouse ? (
+          <CardDescription className="flex justify-between">
+            <span>На складе</span>
+            <span>{numberInWarehouse} шт</span>
+          </CardDescription>
+        ) : null}
+        <div className="flex justify-between items-end">
+          <CardDescription>Стоимость</CardDescription>
+          <div className="flex text-lg font-semibold">{price} р</div>
         </div>
       </CardContent>
       <CardFooter className="justify-center">{children}</CardFooter>
