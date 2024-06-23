683ceb164e109a878be90c1f0b3ae69eac44ff4a
zenkovichso <zenkovich_so@mail.ru>
2024-06-23 08:27:03 +0300

    import removed

--- src/components/sign-up/sign-up-form.tsx
+++ src/components/sign-up/sign-up-form.tsx
@@ -2,7 +2,6 @@
 
 import { z } from "zod";
 import { useState } from "react";
-import { createUser } from "@/service/user/actions";
 import { useToast } from "../ui/use-toast";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
